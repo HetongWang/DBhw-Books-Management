4d73c4ff6e643d3efed650c0b90f0902c1793821
Hetong <hetong583@qq.com>
2015-04-22 17:26:57 +0800

    fix global url bug

--- books/views.py
+++ books/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+
+from .models import Books
 
 # Create your views here.
 def index(request):
     return render(request, 'books/index.html', {})
 
+def search(request):
+    book = Books.objects.get(name=request.POST['content']
+    
+	return HttpResponse('hello')
