410fd1888dac8544246cdf5d94eea61ddd2c2072
Hetong <hetong583@qq.com>
2015-04-27 22:56:29 +0800

    make ui elegant. But my code is ugly T T

--- books/static/books/js/index.js
+++ books/static/books/js/index.js
@@ -1,5 +1,6 @@
 $(function() {
-    $('#search-submit').bind('click', function() {
+    $('#search-submit').bind('click', function(e) {
+    	e.preventDefault();
 		var search_content = $('#search-content').val();
 		if (search_content == '')
 			$('#message').text('You have to type something').css('display', 'block');
